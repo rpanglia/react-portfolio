0cee8e8130071f0aad942d294afac40d7e4c001c
Ravneet Panglia <ravneet.panglia@gmail.com>
2022-01-27 03:00:48 -0500

    home/index.js updated, stylesheets added to index.html, swiper (mobile touch slider) added

---
+++ src/components/Home/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Home() {
+    return (
+        <section className="hero">
+
+            <div className="icon-list">
+                <h1 className="titles">Hello World,</h1>
+                <h2 className="titles">Ravneet here! Reporting from Toronto, Ontario. Nice to e-meet you. </h2>  
+            </div>
+
+        </section>    
+    )
+}
+
+
+export default Home;
+
+// KEEP this page
