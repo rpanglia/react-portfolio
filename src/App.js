bf6be767d32a4f0fdf1e98320ff548289ae9110a
Ravneet Panglia <ravneet.panglia@gmail.com>
2022-01-27 01:24:47 -0500

    bootstrap pkgs added, updated app.js and about/index.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Nav from './components/Nav';
 import About from './components/About';
 
 function App() {
+
+  const [currentPage, handlePageChange] = useState("Home");
+  const renderPage = () => {
+      switch (currentPage) {
+          case "About":
+              return <About />;
+          case "Projects":
+              return <Projects />;
+          case "Resume":
+              return <Resume />;
+          case "Contact":
+              return <Contact />;
+          default:
+              return <Home />;
+      }
+  }
+  
+  const year = new Date().getFullYear()
+
   return (
-    <div className="App">
-      <Nav></Nav>
-      
+    <div>
+      <main>
+          <header>
+              <Nav
+                currentPage={currentPage}
+                handlePageChange={handlePageChange}
+              />
+          </header>
+          <div>{renderPage(currentPage)}</div>
+          
+          <footer>
+            Ravneet Panglia &#169; {year} 
+          </footer>
+      </main>
     </div>
-  );
+  )
 }
 
 export default App;
--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,11 +1,36 @@
 import React from "react";
 
 function About() {
-    
+
     return (
-      <section>
-        <h1 id="about">Ravneet Panglia</h1>
+      <section className="mb-5">
+
+        <h1 className="titles">About Me</h1>
+        <hr></hr>
+
+        <div id="container">
+          <div className="img-text-box"> 
+              <img src="../../../ravimg.jpg" alt="Ravneet's Headshot" className="margin-block"/>
+                <div className="mb-2 margin-block">
+                  <p>
+                    I am a Kinesiologist turned Web Developer.
+                  </p>
+                  <p>
+                    I completed a Bachelor of Science in Human Kinetics and have experience working withinin Brain Injury rehab: specifically focused on return-to-work and improving quality of life treatment. With a passion for healthcare, I am in pursuit of furthering my skillset in a unique way by levering technology to one day improve lives. 
+                  </p>
+                  <p>
+                    I am on track to earn a Full-Stack Web Development certificate from the University of Toronto's School of Continuing Studies.
+                  </p>
+                  <p>
+                    When I'm not in front of screens, I enjoy DIY-ing things around the house, gardening, exploring the world and making memories with friends and family.
+                  </p>
+                </div>
+          </div>
+        </div>
+
       </section>
+
+
     );
   }
   
